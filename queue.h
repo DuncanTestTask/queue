9ff417b3480f3f1df46c76a75a380d92fd96724a
Maxim Surtaev <surtaevm+duncan@gmail.com>
2025-01-28 05:47:47 +1300

    :books: issues #7. add code comments

--- queue.h
+++ queue.h
@@ -12,7 +12,36 @@ struct queue {
 				// 3 - get fail, queue is empty
 };
 
+/**
+ * @brief Initialize a queue structure with a specified size.
+ *
+ * @param size The maximum number of elements the queue can hold.
+ * @return A pointer to the initialized queue structure.
+ *         If memory allocation fails, the error field is set to 1.
+ */
 struct queue *queue_init(size_t size);
+
+/**
+ * @brief Frees the memory allocated for the queue structure.
+ *
+ * @param queue A pointer to the queue structure to be freed.
+ */
 void queue_free(struct queue *queue);
+
+/**
+ * @brief Adds an item to the queue.
+ *
+ * @param queue A pointer to the queue structure.
+ * @param item The item to be added to the queue.
+ *         If not enough free space, the error field is set to 2.
+ */
 void queue_add(struct queue *queue, int item);
+
+/**
+ * @brief Retrieves and removes an item from the queue.
+ *
+ * @param queue A pointer to the queue structure.
+ * @return The item retrieved from the queue.
+ *         If the queue is empty, the error field is set to 3.
+ */
 int queue_get(struct queue *queue);
