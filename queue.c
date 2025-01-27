5cbe81665cdc29642684c17817e2b392ac171746
Maxim Surtaev <surtaevm+duncan@gmail.com>
2025-01-27 21:33:48 +1300

    :construction: issues #1. Working on MVP. add debug output

--- queue.c
+++ queue.c
@@ -1,12 +1,31 @@
 #include "queue.h"
+#if defined(DEBUG)
+#include <stdio.h>
+#define DEBUG_PRINTF(x) printf x
+#else
+#define DEBUG_PRINTF(x)
+#endif
 
 struct queue *queue_init(size_t size) {
-  struct queue *return_value;
-  return return_value;
+	struct queue *return_value = calloc(1, sizeof(struct queue));
+	return_value->data = calloc(size, sizeof(size));
+	return_value->low = return_value->high = size - 1;
+	return_value->max = size;
+	DEBUG_PRINTF(("DEBUG: queue_init size=%ld\n", size));
+	return return_value;
 }
-void queue_free(struct queue *queue) {}
-void queue_add(struct queue *queue, int item) {}
+void queue_free(struct queue *queue) {
+	DEBUG_PRINTF(("DEBUG: queue_free\n"));
+	free(queue->data);
+	free(queue);
+}
+
+void queue_add(struct queue *queue, int item) {
+    DEBUG_PRINTF(("DEBUG: queue_add begin capacity=%d/%d rang=%d/%d\n", queue->count, queue->max, queue->low, queue->high));
+
+}
 int queue_get(struct queue *queue) {
-  int return_value;
-  return return_value;
+    DEBUG_PRINTF(("DEBUG: queue_get begin capacity=%d/%d rang=%d/%d\n", queue->count, queue->max, queue->low, queue->high));
+	int return_value=0;
+	return return_value;
 }
--- queue.h
+++ queue.h
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-
 struct queue {
-        int *data;
-        int low;
-        int high;
-        int count;
-        int max;
+        int *data; // stored data
+        int low; // low mark, extends on left on add
+        int high; // hight mark, reduces on left on pop
+        int count; // curent amount elements
+        int max; //max number elements of the queue
 };
 
 struct queue *queue_init (size_t size);
